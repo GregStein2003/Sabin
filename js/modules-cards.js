64693aef7db8ffa37f57b1027baf66e4da33436f
GregStein2003 <gregorystein10@hotmail.com>
2023-01-26 19:36:19 -0300

    Iniciado estilizacao interna de cards modulos

--- js/modules-cards.js
+++ js/modules-cards.js
@@ -1,10 +1,12 @@
 const card = document.querySelectorAll(".js-modules-card");
-const cardContainer = document.querySelector(".modules-cards__container")
+const cardContainer = document.querySelectorAll(".modules-cards__container")
+const actionBack = document.querySelectorAll(".js-action-back")
 
 card.forEach(function(element){
-    element.addEventListener("click", function(){
-        cardContainer.classList.add("modules-cards__container--active");
+    element.addEventListener("click", function(insideElement){
+        element.parentNode.classList.add("modules-cards__container--active");
         element.classList.add("card--active")
+
+        console.log(insideElement)
     })
 })
-
