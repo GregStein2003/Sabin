5f9cc397bec421de6c91828835066a4e8082051e
GregStein2003 <gregorystein10@hotmail.com>
2023-01-27 19:33:00 -0300

    Finalizado funcionalidade de cards e modificado ordem de elementos no carousel

--- js/modules-cards.js
+++ js/modules-cards.js
@@ -1,12 +1,13 @@
 const card = document.querySelectorAll(".js-modules-card");
-const cardContainer = document.querySelectorAll(".modules-cards__container")
-const actionBack = document.querySelectorAll(".js-action-back")
 
 card.forEach(function(element){
-    element.addEventListener("click", function(insideElement){
+    element.addEventListener("click", function(e){
         element.parentNode.classList.add("modules-cards__container--active");
         element.classList.add("card--active")
 
-        console.log(insideElement)
+        if(e.target.classList.value.includes("js-action-back") == true) {
+            element.parentNode.classList.remove("modules-cards__container--active");
+            element.classList.remove("card--active")
+        }
     })
 })
